355ca77119fbf58dd2304b312d55a303b2380dd3
Sam Westmoreland <samwestmoreland@192.168.1.115>
2024-07-12 21:32:44 +0100

    Trim whitespace

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 
 	"golang.org/x/net/html"
 )
@@ -26,7 +27,12 @@ func main() {
 	var f func(*html.Node)
 	f = func(n *html.Node) {
 		if n.Type == html.ElementNode && n.Data == "a" {
-			log.Println("Found link:", n.FirstChild.Data)
+			trimmed := strings.TrimSpace(n.FirstChild.Data)
+			if trimmed == "" {
+				return
+			}
+
+			log.Println("Found link:", trimmed)
 		}
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
 			f(c)
