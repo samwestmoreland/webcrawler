9c8696893b48b0fc1077d0f0355d11d5e651a2a7
Sam Westmoreland <samwestmoreland@192.168.1.115>
2024-07-13 15:35:43 +0100

    func to extract all links

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"strings"
 
 	"golang.org/x/net/html"
 )
 
 func main() {
-	url := "https://monzo.com"
-
-	log.Println("Visiting", url)
-
-	doc, err := fetch(url)
+	startURL := "https://monzo.com"
+	parsedURL, err := url.Parse(startURL)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	log.Println("Visiting", startURL)
+	log.Println("Host:", parsedURL.Host)
+
+	doc, err := fetch(startURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	links, err := extractLinks(doc, parsedURL.Host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, link := range links {
+		log.Println(link)
+	}
+}
+
+func extractLinks(doc *html.Node, host string) ([]string, error) {
+	var links []string
+	var (
+		invalidLinksCount int
+		validLinksCount   int
+		erroredCount      int
+	)
+	seen := make(map[string]struct{})
 
 	var f func(*html.Node)
 	f = func(n *html.Node) {
@@ -27,7 +54,30 @@ func main() {
 					continue
 				}
 
-				log.Println("Found link:", a.Val)
+				u, err := url.Parse(a.Val)
+				if err == nil && u.Host == host {
+					if _, ok := seen[a.Val]; ok {
+						continue
+					}
+					seen[a.Val] = struct{}{}
+					links = append(links, a.Val)
+					validLinksCount++
+
+				} else if err == nil && u.Host == "" && strings.HasPrefix(a.Val, "/") {
+					// This is a relative link
+					if _, ok := seen[a.Val]; ok {
+						continue
+					}
+					seen[a.Val] = struct{}{}
+					links = append(links, a.Val)
+					validLinksCount++
+				} else if err == nil {
+					log.Printf("Invalid URL: %q u.Host: %q", a.Val, u.Host)
+					invalidLinksCount++
+				} else {
+					log.Println("Invalid URL:", a.Val)
+					erroredCount++
+				}
 			}
 
 		}
@@ -37,9 +87,13 @@ func main() {
 		}
 	}
 	f(doc)
-}
 
-func visitLinks(
+	log.Println("Invalid links count:", invalidLinksCount)
+	log.Println("Valid links count:", validLinksCount)
+	log.Println("Errored count:", erroredCount)
+
+	return links, nil
+}
 
 func fetch(url string) (*html.Node, error) {
 	resp, err := http.Get(url)
@@ -47,6 +101,10 @@ func fetch(url string) (*html.Node, error) {
 		return nil, err
 	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("error: status code %d", resp.StatusCode)
+	}
 
 	doc, err := html.Parse(resp.Body)
 	if err != nil {
